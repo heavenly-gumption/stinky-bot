e49be1ae88ca9b028f3392346d252f4d3c68b3cb
Chudooder <codeshawnachu@gmail.com>
2019-11-03 00:29:33 -0700

    Add money table/model/module

---
+++ src/modules/money.module.ts
@@ -0,0 +1,34 @@
+import { BotModule } from "../types"
+import { getBalance, initUser } from "../types/models/moneybalance"
+import { User, Message, PartialMessage, Client } from "discord.js"
+import pgPromise from "pg-promise"
+
+function printBalance(message: Message | PartialMessage) {
+    if (!message.author || !message.channel) {
+        return
+    }
+
+    const author: User = message.author
+
+    getBalance(author.id)
+        .then(async data => {
+            if (message.channel) {
+                await message.channel.send(author.username + " has **" + data.amount + "** :gem: ")
+            }
+        })
+        .catch(error => {
+            if (error instanceof pgPromise.errors.QueryResultError) {
+                initUser(author.id)
+                    .then(amt => printBalance(message))
+            }
+        })
+}
+
+export const MoneyModule: BotModule = (client: Client) => {
+    console.log("Loaded MoneyModule")
+    client.on("message", async message => {
+        if (message.content && message.content.startsWith("!money")) {
+            printBalance(message)
+        }
+    })
+}
