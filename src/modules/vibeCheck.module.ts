3cc220b102b6702aaeba1d2e5b255af066205d3e
Chris <chris386chris@gmail.com>
2019-11-06 12:57:38 -0500

    We vibin

--- src/modules/vibeCheck.module.ts
+++ src/modules/vibeCheck.module.ts
@@ -5,8 +5,16 @@ export const VibeCheckModule: BotModule = (client: Client) => {
     console.log("Loaded VibeCheckModule")
     client.on("message", async message => {
         if (message.channel && message.content === "vibe check") {
-            const vibeCheck = 20
-            await message.channel.send("Current Vibe: `" + Math.floor(Math.random() * Math.floor(vibeCheck) + 1) + "`")
+            let vibe = Math.floor(Math.random() * 420 + 1)
+            let message = ""
+            if (vibe === 420) {
+            	message = "Dank Vibe: `" + vibe + "`"
+            } else if (vibe === 69) {
+            	message = "Sexy Wexy Vibey Wibey UwU: `" + vibe + "`"
+            } else {
+            	message = "Current Vibe: `" + (Math.floor(vibe / 21) + 1) + "`"
+            }
+            await message.channel.send(message)
         }
     })
 }
