ce88853e25f54db9c1440aafefec933b16c24550
Shawn Wu <codeshawnachu@gmail.com>
2021-01-14 14:37:15 -0800

    Mathjax support (#26)
    * Add libgc1c2 to Aptfile
    
    * Fix inkscape command line options
    
    * Upgrade inkscape maybe
    
    * Try again xd
    
    * For real this time
    
    * Downgraded to 0.92 commands
    
    * Whoops

--- src/scripts/svg2png.sh
+++ src/scripts/svg2png.sh
@@ -1,9 +1,10 @@
 SVG_NAME=$1
 TEMP_DIRECTORY="$2"
 FILEPATH="${TEMP_DIRECTORY}/${SVG_NAME}.svg"
+OUTFILEPATH="${TEMP_DIRECTORY}/${SVG_NAME}.png"
 echo "${FILEPATH}"
 inkscape \
-    --export-type=png \
+    --export-png="${OUTFILEPATH}" \
     --export-dpi=100 \
     --export-background="#ffffffff" \
     "${FILEPATH}"
