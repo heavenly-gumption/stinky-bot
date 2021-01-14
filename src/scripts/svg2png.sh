d0cc09b80105b950328158285b92527298d8a60e
Chudooder <codeshawnachu@gmail.com>
2021-01-14 13:52:34 -0800

    Upgrade inkscape maybe

--- src/scripts/svg2png.sh
+++ src/scripts/svg2png.sh
@@ -3,7 +3,7 @@ TEMP_DIRECTORY="$2"
 FILEPATH="${TEMP_DIRECTORY}/${SVG_NAME}.svg"
 echo "${FILEPATH}"
 inkscape \
-    --export-png \
+    --export-type=png \
     --export-dpi=100 \
     --export-background="#ffffffff" \
     "${FILEPATH}"
