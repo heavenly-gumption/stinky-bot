3a5f7308de088f1bebd690b13559bfa97fc7d72c
Michael Lion <mlin4589@gmail.com>
2021-01-28 22:37:28 -0500

    Updated logic for ssl verification Removed dev flags

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,24 +1,16 @@
 import pgPromise from "pg-promise"
 
-// Disable SSL if in development mode
-function getDbString(): string {
-    const baseURL = process.env.DATABASE_URL ? process.env.DATABASE_URL: ""
-    if (process.env.NODE_ENV === "development") {
-        return baseURL
-    } else {
-        return baseURL + "?ssl=true"
-    }
-}
-const dbUrl: string = getDbString()
+const baseURL = process.env.DATABASE_URL ? process.env.DATABASE_URL: ""
 
-// Skip SSL verification if the environment is dev
+// See: https://help.heroku.com/MDM23G46/why-am-i-getting-an-error-when-i-upgrade-to-pg-8
+// Skip SSL verification
 const ssl = {
-    rejectUnauthorized: process.env.NODE_ENV !== "development"
+    rejectUnauthorized: false
 }
 
 const pgp = pgPromise({})
 const db = pgp({
-    connectionString: dbUrl,
+    connectionString: baseURL,
     ssl: ssl
 })
 
