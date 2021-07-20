bfd5df4bbda70bb526b23b1ae1bae4412f22bdb8
Shawn Wu <codeshawnachu@gmail.com>
2021-07-20 11:41:17 -0700

    Add baltop and pay commands (#39)
    * Add baltop command
    
    * Add pay

--- src/types/models/firestore/moneybalance.ts
+++ src/types/models/firestore/moneybalance.ts
@@ -1,9 +1,20 @@
 import { getFirestoreConnection, getOne } from "../../../utils/db/firestore"
-import { MoneyBalance, MoneyBalanceDao, MONEY_COLLECTION_NAME } from '../moneybalance.dao'
+import { MoneyBalance, MoneyBalanceDao, MONEY_COLLECTION_NAME,
+    BALANCE_UNINITIALIZED_ERROR, LOW_BALANCE_ERROR } from '../moneybalance.dao'
 
 function getBalance(user: string): Promise<MoneyBalance> {
     const moneyRef = getFirestoreConnection().collection(MONEY_COLLECTION_NAME)
     return getOne<MoneyBalance>(moneyRef, user)
+}
+
+async function getAllBalances(): Promise<Map<string, MoneyBalance>> {
+    const moneyRef = getFirestoreConnection().collection(MONEY_COLLECTION_NAME)
+    const snapshot = await moneyRef.get()
+    const res: Map<string, MoneyBalance> = new Map()
+    snapshot.forEach(doc => {
+        res.set(doc.id, doc.data() as MoneyBalance)
+    })
+    return res
 }
 
 async function initUser(user: string): Promise<void> {
@@ -11,7 +22,32 @@ async function initUser(user: string): Promise<void> {
     await moneyRef.doc(user).set({ id: user, amount: 10000 })
 }
 
+async function transfer(from: string, to: string, amount: number): Promise<void> {
+    const db = getFirestoreConnection()
+    const fromDocRef = db.collection(MONEY_COLLECTION_NAME).doc(from)
+    const toDocRef = db.collection(MONEY_COLLECTION_NAME).doc(to)
+
+    await db.runTransaction(async (t) => {
+        const fromDoc = await t.get(fromDocRef)
+        const toDoc = await t.get(toDocRef)
+        if (!fromDoc.exists || !toDoc.exists) {
+            throw BALANCE_UNINITIALIZED_ERROR
+        }
+
+        const fromMoney: MoneyBalance = fromDoc.data() as MoneyBalance
+        const toMoney: MoneyBalance = toDoc.data() as MoneyBalance
+        if (fromMoney.amount < amount) {
+            throw LOW_BALANCE_ERROR
+        }
+
+        t.update(fromDocRef, { amount: fromMoney.amount - amount })
+        t.update(toDocRef, { amount: toMoney.amount + amount })
+    })
+}
+
 export const MoneyBalanceFirestoreDao: MoneyBalanceDao = {
     getBalance,
-    initUser
+    getAllBalances,
+    initUser,
+    transfer
 }
